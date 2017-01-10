b1624ce3971236737f7f76b9d077305763ebb009
s9w <swerhausen@googlemail.com>
2017-01-10 14:38:24 +0100

    added 24h indicator for posts

--- static/script.js
+++ static/script.js
@@ -1,3 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(){
+  // var now = moment();
+
+  var yday = moment().subtract(1, 'days');
+  var readElArray = document.getElementsByTagName("ul");
+    for(var i = readElArray.length-1; i>=0; i--){
+        var ulElement = readElArray[i];
+        for(var j = 0; j < ulElement.children.length; j++){
+            var liElement = ulElement.children[j];
+            var mom = moment.unix(liElement.dataset.date);
+
+            if(mom.isBefore(yday)){
+                // Looks silly before first element. In that case: no indication at all
+                if(j===0){
+                    break;
+                }
+
+                liElement.classList.add("time_24h_margin");
+
+                var newNode = document.createElement("div");
+                newNode.appendChild(document.createTextNode("↓ 24 hours ↓"));
+                newNode.classList.add("time_24h");
+                // ulElement.insertBefore(newNode, liElement);
+                liElement.appendChild(newNode);
+                break;
+            }
+
+            // console.log("li, " + readElArray[i].children[j].dataset.date);
+        }
+    }
+});
+
 function mark_read(el, type, subtype, timestamp){
     for(var li = el.parentElement; li; li=li.nextElementSibling){
         li.classList.add("read");
@@ -27,8 +59,7 @@ function mark_read_temp(el){
 
 // undos the read marking
 function undo_mark_read_temp(el){
-    var readElArray = document.getElementsByClassName("read_temp");
-    for(var i = readElArray.length-1; i>=0; i--){
-        readElArray[i].classList.remove("read_temp");
+    for(var li = el.parentElement; li; li=li.nextElementSibling){
+        li.classList.remove("read_temp");
     }
 }
